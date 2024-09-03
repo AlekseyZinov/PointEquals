c2e0a453d90fe679a5d8ec8ed0b1a6eee2397c82
Aleksey Zinov <AZinov@inno.tech>
2024-09-03 23:08:33 +0300

    added class polyline

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,12 @@ public class Main {
     public static void main(String[] args) throws CloneNotSupportedException {
         Point p1 = new Point(1,3);
         Point p2 = new Point(2,5);
+        Point p3 = new Point(4,8);
+        Point p4 = new Point(5,9);
 
-        Point p3 = new Point(1,3);
-        Point p4 = new Point(2,5);
+        PolyLine pl1 = new PolyLine(p1, p2, p3, p4);
+        PolyLine pl2 = new PolyLine(p1, p2, p3, p4);
 
-        Line line1 = new Line(p1, p2);
-        Line line2 = new Line(p3, p4);
-
-        Line line3 = line2.clone();
-
-        System.out.println(line1.equals(line2));
-        System.out.println(line2.equals(line3));
+        System.out.println(pl1.equals(pl2));
     }
 }
